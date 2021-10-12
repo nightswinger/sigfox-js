b0ac200d34c01ef6f27f02186980dfedfd3dc757
nightswinger <stardustkids83@gmail.com>
2021-10-12 09:08:06 +0900

    update types

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,5 @@
-import HTTPClient from './http'
+import { DeviceMessage, Pagination } from ".";
+import HTTPClient from "./http";
 import {
   createDeviceParams,
   Device,
@@ -8,97 +9,109 @@ import {
   deviceTypeList,
   transferMultipleDevicesParams,
   updateDeviceParams,
-  updateDeviceTypeParams
-} from './types'
-import { btoa } from './utils'
+  updateDeviceTypeParams,
+} from "./types";
+import { btoa } from "./utils";
 
 export type ClientConfig = {
-  username: string,
-  password: string
-}
+  username: string;
+  password: string;
+};
 
 export default class Client {
-  public config: ClientConfig
-  private http: HTTPClient
+  public config: ClientConfig;
+  private http: HTTPClient;
 
   constructor(config: ClientConfig) {
     if (!config.username || !config.password) {
-      throw new Error("username or password doesn't exist")
+      throw new Error("username or password doesn't exist");
     }
-    this.config = config
+    this.config = config;
     this.http = new HTTPClient({
-      defaultHeaders: this.authHeader()
-    })
+      defaultHeaders: this.authHeader(),
+    });
   }
 
   public devices(params: devicesQueryParams = {}): Promise<deviceList> {
-    return this.http.get('/devices', params)
+    return this.http.get("/devices", params);
   }
 
   public createDevice(params: createDeviceParams): Promise<any> {
-    return this.http.post('/devices', params)
+    return this.http.post("/devices", params);
   }
 
   public getDevice(deviceId: string): Promise<Device> {
-    return this.http.get(`/devices/${deviceId}`)
+    return this.http.get(`/devices/${deviceId}`);
   }
 
   public updateDevice(deviceId: string, params: updateDeviceParams) {
-    return this.http.put(`/devices/${deviceId}`, params)
+    return this.http.put(`/devices/${deviceId}`, params);
   }
 
   public deleteDevice(deviceId: string): Promise<any> {
-    return this.http.delete(`/devices/${deviceId}`)
+    return this.http.delete(`/devices/${deviceId}`);
   }
 
   public getUndeliveredCallbacks(deviceId: string): Promise<any> {
-    return this.http.get(`/devices/${deviceId}/callbacks-not-delivered`)
+    return this.http.get(`/devices/${deviceId}/callbacks-not-delivered`);
   }
 
   public disengageSequenceNumber(deviceId: string): Promise<any> {
-    return this.http.post(`/devices/${deviceId}/disengage`)
+    return this.http.post(`/devices/${deviceId}/disengage`);
   }
 
-  public getMessages(deviceId: string): Promise<any> {
-    return this.http.get(`/devices/${deviceId}/messages`)
+  public getMessages(
+    deviceId: string
+  ): Promise<{ data?: DeviceMessage[]; paging?: Pagination }> {
+    return this.http.get(`/devices/${deviceId}/messages`);
   }
 
   public getNumberOfMessages(deviceId: string): Promise<any> {
-    return this.http.get(`/devices/${deviceId}/messages/metric`)
+    return this.http.get(`/devices/${deviceId}/messages/metric`);
   }
 
   public getDeviceLocations(deviceId: string): Promise<any> {
-    return this.http.get(`/devices/${deviceId}/locations`)
+    return this.http.get(`/devices/${deviceId}/locations`);
   }
 
   public deviceTypes(): Promise<deviceTypeList> {
-    return this.http.get('/device-types')
+    return this.http.get("/device-types");
   }
 
-  public transferMultipleDevices(params: transferMultipleDevicesParams): Promise<any> {
-    return this.http.post('/devices/bulk/transfer', params)
+  public transferMultipleDevices(
+    params: transferMultipleDevicesParams
+  ): Promise<any> {
+    return this.http.post("/devices/bulk/transfer", params);
   }
 
   public getDeviceType(deviceTypeId: string): Promise<DeviceType> {
-    return this.http.get(`/device-types/${deviceTypeId}`)
+    return this.http.get(`/device-types/${deviceTypeId}`);
   }
 
-  public updateDeviceType(deviceTypeId: string, params: updateDeviceTypeParams): Promise<any> {
-    return this.http.put(`/device-types/${deviceTypeId}`, params)
+  public updateDeviceType(
+    deviceTypeId: string,
+    params: updateDeviceTypeParams
+  ): Promise<any> {
+    return this.http.put(`/device-types/${deviceTypeId}`, params);
   }
 
   public getMessagesByDeviceType(deviceTypeId: string): Promise<any> {
-    return this.http.get(`/device-types/${deviceTypeId}/messages`)
+    return this.http.get(`/device-types/${deviceTypeId}/messages`);
   }
 
-  public getCallbacksNotDelivered(deviceTypeId: string, callbackId: string): Promise<any> {
-    return this.http.get(`/device-types/${deviceTypeId}/callbacks/${callbackId}/callbacks-not-delivered`)
+  public getCallbacksNotDelivered(
+    deviceTypeId: string,
+    callbackId: string
+  ): Promise<any> {
+    return this.http.get(
+      `/device-types/${deviceTypeId}/callbacks/${callbackId}/callbacks-not-delivered`
+    );
   }
 
   private authHeader() {
-    const { username, password } = this.config
-    const basicAuth = 'Basic ' + btoa(username + ':' + password)
+    const { username, password } = this.config;
+    const basicAuth = "Basic " + btoa(username + ":" + password);
 
-    return { Authorization: basicAuth }
+    return { Authorization: basicAuth };
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,180 +1,228 @@
+export type ComputedLocation = {
+  lat?: number;
+  lng?: number;
+  radius?: number;
+  source?: number;
+  placeIds?: string[];
+};
+
 export type Device = {
-  id: string,
-  name: string,
-  satelliteCapable?: boolean,
-  repeater?: boolean,
-  messageModulo?: number,
-  deviceType?: MinimalDeviceType,
-  contract?: MinimalContract,
-  group?: MinimalGroup,
-  modemCertificate?: Certificate,
-  prototype?: boolean,
-  productCertificate?: Certificate,
-  location?: DeviceLocation,
-  lastComputedLocation?: LastComputedLocation,
-  pac: string,
-  sequenceNumber?: number,
-  trashSequenceNumber?: number,
-  lastCom?: number,
-  lqi?: number,
-  activationTime?: number,
-  creationTime: number,
-  state: number,
-  comState: number,
-  token?: Token,
-  unsubscriptionTime?: number,
-  createdBy?: string,
-  lastEditionTime?: number,
-  lastEditedBy?: string,
-  automaticRenewal: boolean,
-  automaticRenewalStatus?: number,
-  activable?: boolean,
-  actions?: string[],
-  resources?: string[]
-}
+  id: string;
+  name: string;
+  satelliteCapable?: boolean;
+  repeater?: boolean;
+  messageModulo?: number;
+  deviceType?: MinimalDeviceType;
+  contract?: MinimalContract;
+  group?: MinimalGroup;
+  modemCertificate?: Certificate;
+  prototype?: boolean;
+  productCertificate?: Certificate;
+  location?: DeviceLocation;
+  lastComputedLocation?: LastComputedLocation;
+  pac: string;
+  sequenceNumber?: number;
+  trashSequenceNumber?: number;
+  lastCom?: number;
+  lqi?: number;
+  activationTime?: number;
+  creationTime: number;
+  state: number;
+  comState: number;
+  token?: Token;
+  unsubscriptionTime?: number;
+  createdBy?: string;
+  lastEditionTime?: number;
+  lastEditedBy?: string;
+  automaticRenewal: boolean;
+  automaticRenewalStatus?: number;
+  activable?: boolean;
+  actions?: string[];
+  resources?: string[];
+};
+
+export type DeviceMessage = {
+  device?: any;
+  time?: number;
+  data?: string;
+  ackRequired?: boolean;
+  lqi?: number;
+  lqiRepeaters?: number;
+  seqNumber?: number;
+  nbFrames?: number;
+  computedLocation?: ComputedLocation[];
+  rinfos?: Rinfo[];
+  donwlinkAnswerStatus?: DownlinkAnswerStatus;
+};
 
 export type DeviceType = {
-  name?: string,
-  description?: string,
-  downlinkMode?: number,
-  downlinkDataString?: string,
-  payloadType?: number,
-  payloadConfig?: string,
-  keepAlive?: number,
-  alertEmail?: string,
-  automaticRenewal?: boolean,
-  id?: string,
-  group?: object,
-  contract?: object,
-  contracts?: object[],
-  detachedContracts?: object[],
-  geolocPayloadConfig?: object,
-  creationTime?: number,
-  createdBy?: string,
-  lastEditionTime?: number,
-  lastEditedBy?: string
-}
+  name?: string;
+  description?: string;
+  downlinkMode?: number;
+  downlinkDataString?: string;
+  payloadType?: number;
+  payloadConfig?: string;
+  keepAlive?: number;
+  alertEmail?: string;
+  automaticRenewal?: boolean;
+  id?: string;
+  group?: object;
+  contract?: object;
+  contracts?: object[];
+  detachedContracts?: object[];
+  geolocPayloadConfig?: object;
+  creationTime?: number;
+  createdBy?: string;
+  lastEditionTime?: number;
+  lastEditedBy?: string;
+};
+
+export type DownlinkAnswerStatus = {
+  baseStation?: any;
+  plannedPower?: number;
+  data?: string;
+  operator?: string;
+  country?: string;
+};
+
+export type Pagination = {
+  next?: string;
+};
+
+export type Rinfo = {
+  baseStation?: any;
+  rssi?: number;
+  rssiRepeaters?: number;
+  lat?: number;
+  lng?: number;
+  delay?: number;
+  freq?: number;
+  freqRepeaters?: number;
+  rep?: number;
+  repetitions?: any[];
+  cbStatus?: any[];
+};
 
 export type MinimalDeviceType = {
-  id?: string,
-  name?: string,
-  actions?: string[],
-  resources?: string[]
-}
+  id?: string;
+  name?: string;
+  actions?: string[];
+  resources?: string[];
+};
 
 export type MinimalContract = {
-  id?: string,
-  name?: string,
-  actions?: string[],
-  resources?: string[]
-}
+  id?: string;
+  name?: string;
+  actions?: string[];
+  resources?: string[];
+};
 
 export type MinimalGroup = {
-  id?: string,
-  name?: string,
-  type?: number,
-  level?: number,
-  actions?: string[],
-  resources?: string[]
-}
+  id?: string;
+  name?: string;
+  type?: number;
+  level?: number;
+  actions?: string[];
+  resources?: string[];
+};
 
 export type Certificate = {
-  id?: string,
-  key?: string
-}
+  id?: string;
+  key?: string;
+};
 
 export type DeviceLocation = {
-  lat?: number,
-  lng?: number
-}
+  lat?: number;
+  lng?: number;
+};
 
 export type LastComputedLocation = {
-  lat?: number,
-  lng?: number,
-  radius?: number,
-  sourceCode?: number,
-  placeIds?: string[]
-}
+  lat?: number;
+  lng?: number;
+  radius?: number;
+  sourceCode?: number;
+  placeIds?: string[];
+};
 
 export type Token = {
-  state?: number,
-  detailMessage?: string,
-  end?: number,
-  freeMessages?: number,
-  freeMessagesSent?: number
-}
+  state?: number;
+  detailMessage?: string;
+  end?: number;
+  freeMessages?: number;
+  freeMessagesSent?: number;
+};
 
 export type deviceList = {
-  data?: Device[],
-  actions?: string[],
-  paging?: object
-}
+  data?: Device[];
+  actions?: string[];
+  paging?: object;
+};
 
 export type deviceTypeList = {
-  data?: DeviceType[],
-  actions?: string[],
-  paging?: object
-}
+  data?: DeviceType[];
+  actions?: string[];
+  paging?: object;
+};
 
 export type devicesQueryParams = {
-  id?: string,
-  groupIds?: string[],
-  deep?: boolean,
-  authorizations?: boolean,
-  deviceTypeId?: string,
-  operatorId?: string,
-  sort?: string,
-  minId?: string,
-  maxId?: string,
-  fields?: string,
-  limit?: number,
-  offset?: number,
-  pageId?: string
-}
+  id?: string;
+  groupIds?: string[];
+  deep?: boolean;
+  authorizations?: boolean;
+  deviceTypeId?: string;
+  operatorId?: string;
+  sort?: string;
+  minId?: string;
+  maxId?: string;
+  fields?: string;
+  limit?: number;
+  offset?: number;
+  pageId?: string;
+};
 
 export type createDeviceParams = {
-  id: string,
-  name: string,
-  activable?: boolean,
-  automaticRenewal?: boolean,
-  lat?: number,
-  lng?: number,
-  productCertificate?: object,
-  prototype?: boolean,
-  deviceTypeId: string,
-  pac: string
-}
+  id: string;
+  name: string;
+  activable?: boolean;
+  automaticRenewal?: boolean;
+  lat?: number;
+  lng?: number;
+  productCertificate?: object;
+  prototype?: boolean;
+  deviceTypeId: string;
+  pac: string;
+};
 
 export type updateDeviceParams = {
-  activable?: boolean,
-  automaticRenewal?: boolean,
-  lat?: number,
-  lng?: number,
-  productCertificate?: object,
-  prototype?: boolean,
-  name?: string
-}
+  activable?: boolean;
+  automaticRenewal?: boolean;
+  lat?: number;
+  lng?: number;
+  productCertificate?: object;
+  prototype?: boolean;
+  name?: string;
+};
 
 export type transferMultipleDevicesParams = {
-  deviceTypeId: string,
+  deviceTypeId: string;
   data?: {
-    id: string,
-    keepHistory?: boolean,
-    activable?: boolean
-  }[]
-}
+    id: string;
+    keepHistory?: boolean;
+    activable?: boolean;
+  }[];
+};
 
 export type updateDeviceTypeParams = {
-  name?: string,
-  description?: string,
-  donwlinkMode?: number,
-  downlinkDataString?: string,
-  payloadType?: number,
-  payloadConfig?: string,
-  keepAlive?: number,
-  alertEmail?: string,
-  automaticRenewal?: boolean,
-  contractId?: string,
-  contracts?: string[],
-  geolocPayloadConfigId?: string
-}
+  name?: string;
+  description?: string;
+  donwlinkMode?: number;
+  downlinkDataString?: string;
+  payloadType?: number;
+  payloadConfig?: string;
+  keepAlive?: number;
+  alertEmail?: string;
+  automaticRenewal?: boolean;
+  contractId?: string;
+  contracts?: string[];
+  geolocPayloadConfigId?: string;
+};
