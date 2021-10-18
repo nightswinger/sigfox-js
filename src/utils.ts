b9c671469a3e9b230565a5263b49d94a58523a22
nightswinger <stardustkids83@gmail.com>
2021-10-18 17:57:44 +0900

    add mixin method

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,15 @@
 export function btoa(binary: string): string {
   return Buffer.from(binary).toString('base64')
 }
+
+export function mixin(derivedCtor: any, constructors: any[]) {
+  constructors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null)
+      )
+    })
+  })
+}
