3dd3fad42595ea8a234b7bf797472e97f98f0eab
nightswinger <stardustkids83@gmail.com>
2021-10-19 09:06:56 +0900

    add mixin test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,10 +1,28 @@
-import { btoa } from "../src/utils";
+import { btoa, mixin } from "../src/utils";
 
 describe("utils", () => {
   describe("btoa", () => {
     it("returns a base64 encoded ascii string", () => {
       let input = "Hello World!";
       expect(btoa(input)).toEqual("SGVsbG8gV29ybGQh");
+    });
+  });
+
+  describe("mixin", () => {
+    it("compose mixins into base class", () => {
+      class Jumpable {
+        jump() {
+          return "jump";
+        }
+      }
+      class Base {}
+
+      interface Base extends Jumpable {}
+
+      mixin(Base, [Jumpable]);
+
+      let base = new Base();
+      expect(base.jump()).toEqual("jump");
     });
   });
 });
