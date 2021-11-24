a82f12c75bdf4c482e3d32a1d8348287b1d620a9
nightswinger <stardustkids83@gmail.com>
2021-11-24 11:06:42 +0000

    lint

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,32 +1,20 @@
-import { rest } from 'msw'
+import { rest } from "msw";
 
 const sigfox = (path: string) => {
-  return new URL(`/v2${path}`, 'https://api.sigfox.com').toString()
-}
+  return new URL(`/v2${path}`, "https://api.sigfox.com").toString();
+};
 
 export const handlers = [
-  rest.get(sigfox('/get'), (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({})
-    )
+  rest.get(sigfox("/get"), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({}));
   }),
-  rest.post(sigfox('/post'), (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({})
-    )
+  rest.post(sigfox("/post"), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({}));
   }),
-  rest.put(sigfox('/put'), (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({})
-    )
+  rest.put(sigfox("/put"), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({}));
   }),
-  rest.delete(sigfox('/delete'), (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({})
-    )
-  })
-]
+  rest.delete(sigfox("/delete"), (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({}));
+  }),
+];
